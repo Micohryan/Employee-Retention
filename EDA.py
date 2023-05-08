9667b4473bf0e45f64a719e471f87efe8765b603
Micohryan <micohryan@gmail.com>
2023-05-08 13:08:30 -0700

    Added Titles and Labels

--- EDA.py
+++ EDA.py
@@ -15,15 +15,16 @@ corr_matrix = df.corr(numeric_only= True)
 fig, ax = plt.subplots(figsize = (10,10))
 sns.heatmap(corr_matrix, annot=True, ax=ax)
 plt.show()
-def count_plot(data, x:str , hue:str) -> None:
-    sns.countplot(data = data, x = x , hue = hue)
+def count_plot(data, x:str , hue:str, Title:str, labels:list = None) -> None:
+    sns.countplot(data = data, x = x , hue = hue).set(title=Title)
+    plt.legend(labels = labels)
     plt.show()
     return
 
-count_plot(df, x = "JoiningYear", hue = "LeaveOrNot")
-count_plot(df, x = "Education", hue = "LeaveOrNot")
-count_plot(df, x = "EverBenched", hue = "LeaveOrNot")
-count_plot(df, x = "Gender", hue = "LeaveOrNot")
-count_plot(df, x = "Gender", hue = "Education")
-count_plot(df, x = "Age", hue = "LeaveOrNot")
-exit()
+count_plot(df, x = "JoiningYear", hue = "LeaveOrNot",Title = 'Joinning Year vs LeaveorNot', labels=['Left', 'Stayed'])
+count_plot(df, x = "Education", hue = "LeaveOrNot", Title = 'Education vs LeaveorNot', labels=['Left', 'Stayed'])
+count_plot(df, x = "EverBenched", hue = "LeaveOrNot",Title = 'EverBenched vs LeaveorNot', labels=['Left', 'Stayed'])
+count_plot(df, x = "Gender", hue = "LeaveOrNot",Title = 'Gender vs LeaveorNot', labels=['Left', 'Stayed'])
+count_plot(df, x = "Gender", hue = "Education",Title = 'Gender vs Education')
+count_plot(df, x = "Age", hue = "LeaveOrNot",Title = 'Age vs LeaveorNot', labels=['Left', 'Stayed'])
+
